5dbe488044c19b096d946908565ed465bc34711e
jacob <jacobharv00@gmail.com>
2021-08-05 12:58:34 -0600

    fix boolean issue only on jacob's end?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,6 @@ class ApplicationController < Sinatra::Base
   # register Sinatra::CrossOrigin
   set :default_content_type, 'application/json'
   
-  # configure do
-  #   enable :cross_origin
-  #   set :allow_origin, "*" 
-  #   set :allow_methods, [:get, :post, :patch, :delete, :options] # allows these HTTP verbs
-  #   set :expose_headers, ['Content-Type']
-  # end
-
-  # options "*" do
-  #   response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-  #   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-  #   200
-  # end
-
-  # Add your routes here
-
   get "/restaurants" do
     restaurants = Restaurant.all
     restaurants.to_json(include:{reviews:{include: :user}})
